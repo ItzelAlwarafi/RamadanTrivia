4112db8fbb622854ce35929eb004d42b2096c273
itzel <itzelalwarafii@gmail.com>
2024-01-26 10:52:55 -0800

    changebutton/add quenstionsfunctions

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
+
+//html elements ///
+const logo = document.getElementById('logo')
+const welcomeMessage =document.getElementById('welcomeMessage')
+const instructions =document.getElementById('instructions')
+const startGamebtn = document.getElementById('startGamebtn')
+const playBtn = document.getElementById('playBtn')
+
+
+// Trivia questions
+const questions = [
+    { question: "What is the first month of the Islamic calendar?", options: ["Muharram", "Safar", "Rabi' al-Awwal", "Ramadan"], correctAnswer: "Muharram" },
+    { question: "During which month do Muslims fast from sunrise to sunset?", options: ["Shawwal", "Dhu al-Qi'dah", "Ramadan", "Dhu al-Hijjah"], correctAnswer: "Ramadan" },
+    // need to add more questions 
+]
+ 
+let currentQuestionIndex =0
+let score = 0
+
+//funtion to show questions 
+function displayQuestion() {
+    const questionContainer = document.getElementById("questionContainer")
+    const optionsContainer = document.getElementById("optionsContainer")
+    const currentQuestion = questions[currentQuestionIndex]
+
+    questionContainer.innerHTML = `<p>${currentQuestion.question}</p>`
+    optionsContainer.innerHTML = currentQuestion.options.map(option => `<button id ='optionsBtn'onclick="checkAnswer('${option}')">${option}</button>`).join('')
+}
+
+
 function changePage(){
-    const logo = document.getElementById('logo')
     logo.style.width = "300px"
     logo.style.height = "200px"
     logo.style.marginLeft = '0'
-    const welcomeMessage =document.getElementById('welcomeMessage')
     welcomeMessage.style.display ="none"
-    const instructions =document.getElementById('instructions')
     welcomeMessage.textContent = instructions
     instructions.style.opacity ='2'
-    const startGamebtn = document.getElementById('startGamebtn')
+    
     startGamebtn.style.display ="none"
-    const playBtn = document.getElementById('playBtn')
+   
     playBtn.style.opacity ="2"
     
     
    
 }
-function hideBtn(){
+
+
+function startGame(){
+   instructions.style.display ="none"
+ displayQuestion()
+ playBtn.style.display="none"
    
+
 }
+
+
